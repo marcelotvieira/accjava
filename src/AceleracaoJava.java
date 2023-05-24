41256cbf5a692b1ed5caea7750b1d95b37ae5ffa
Marcelo Vieira <marcelodefy15@gmail.com>
2023-05-24 14:14:02 -0300

    Feat - collections handling - streams.

--- src/AceleracaoJava.java
+++ src/AceleracaoJava.java
@@ -1,9 +1,43 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class AceleracaoJava {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		
+		List<Conta> list = new ArrayList<>();
+		list.add(new Conta(1, "Marcelo", 10));
+		list.add(new Conta(2, "Gustavo", 20));
+		list.add(new Conta(3, "Fernanda", 0));
+		list.add(new Conta(4, "Rosangela", 50));
+		list.add(new Conta(5, "Rubens", 30));
+		
+		// collections handling //
+		
+		List<String> listaPorNomeDoCliente = list.stream()
+				.sorted(Comparator.comparing(Conta::getNomeDoCliente))
+				.map(Conta::getNomeDoCliente)
+				.collect(Collectors.toList());
+		
+		 System.out.println("Listados em ordem alfabética:");
+		 System.out.println(listaPorNomeDoCliente); 
+		 System.out.println("*------------------*");
+		//[Fernanda, Gustavo, Marcelo, Rosangela, Rubens]
+		
+		
+		int somaDosScores = list.stream()
+				.map(Conta::getScore)
+				.reduce(0, (acc, curr) -> acc + curr);
+		
+		 System.out.println("Soma do Score das contas:");
+		 System.out.println(somaDosScores);
+		// 110
+				
+		
+
 
 	}
-
 }
--- src/Conta.java
+++ src/Conta.java
@@ -1,4 +1,38 @@
 
 public class Conta {
+	private int id;
+	private String nomeDoCliente;
+	private int score;
+	
+	public Conta(int id, String nome, int score) {
+		this.id = id;
+		this.nomeDoCliente = nome;
+		this.score = score;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNomeDoCliente() {
+		return nomeDoCliente;
+	}
+
+	public void setNomeDoCliente(String nomeDoCliente) {
+		this.nomeDoCliente = nomeDoCliente;
+	}
+	
 
 }
