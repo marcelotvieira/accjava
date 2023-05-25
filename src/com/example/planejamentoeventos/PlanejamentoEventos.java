66a54672d8f4e2771eb55eb4cfe7994363aac2bf
Marcelo Vieira <marcelodefy15@gmail.com>
2023-05-25 17:12:39 -0300

    Feat - adding formatter

--- src/com/example/planejamentoeventos/PlanejamentoEventos.java
+++ src/com/example/planejamentoeventos/PlanejamentoEventos.java
@@ -2,11 +2,14 @@ package com.example.planejamentoeventos;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class PlanejamentoEventos {
 	
 	LocalDate dataEvento;
 	LocalTime horaEvento;
+	String dateFormat = "dd/MM/yyyy";
+	DateTimeFormatter formatadorData = DateTimeFormatter.ofPattern(dateFormat);
 	
 	public void agendarEvento(String data, String hora) {
 		dataEvento = LocalDate.parse(data);
@@ -19,13 +22,17 @@ public class PlanejamentoEventos {
 		
 		if(dataAgora.equals(dataEvento)) {
 			System.out.println("O evento é hoje");
+			System.out.println(formatadorData.format(dataEvento) + " as " + horaEvento);
 			if (horaAgora.isAfter(horaEvento)) {
 				System.out.println("Você ja está atrasado, já passou do inicio do evento");
+				System.out.println(formatadorData.format(dataEvento) + " as " + horaEvento);
 			} else {
 				System.out.println("Se arrume que da tempo!");
+				System.out.println(formatadorData.format(dataEvento) + " as " + horaEvento);
 			}
 		} else {
 			System.out.println("O evento não é hoje!");
+			System.out.println(formatadorData.format(dataEvento) + " as " + horaEvento);
 		}
 	}
 
